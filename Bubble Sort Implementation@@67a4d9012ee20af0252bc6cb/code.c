30463ca0e51bd75c943cbfe99da318251df34da1
monika0208 <mohindroomonika@gmail.com>
2025-03-10 19:37:25 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,7 +1,7 @@
 // Your code here...
 #include<stdio.h>
 
-void bubblesort(int arr[],int n){
+void bubbleSort(int arr[],int n){
     int min=arr[0];
     for(int i=0;i<n;i++){
         if(min>arr[i]){
