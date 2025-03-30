ef3950bbacc9927b281a20852a328e808aa3a787
monika0208 <mohindroomonika@gmail.com>
2025-03-30 22:42:23 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -16,11 +16,17 @@ int main(){
             }
         
         }
-        if(count>n/2){
+        // if(count>n/2){
+        //     printf("%d",arr[i]);
+        // }
+        // else{
+        //     printf("-1");
+        // }
+    }
+    if(count>n/2){
             printf("%d",arr[i]);
         }
         else{
             printf("-1");
         }
-    }
 }
