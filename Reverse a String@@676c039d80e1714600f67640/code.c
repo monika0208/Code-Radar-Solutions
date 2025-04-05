e305a6914c0a9e57e23b40f431b6c302a04a1c5c
monika0208 <mohindroomonika@gmail.com>
2025-04-05 15:28:49 +0530

    Solution to Reverse a String

---
+++ Reverse a String@@676c039d80e1714600f67640/code.c
@@ -0,0 +1,15 @@
+// Your code here...
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    gets(str);
+    int n=strlen(str);
+    for(int i=0,j=n-1;i<=j;i++,j--){
+        int temp=str[i];
+        str[i]=str[j];
+        str[j]=temp;
+    }
+    puts(str);
+    return 0;
+}
